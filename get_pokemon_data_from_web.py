5493898bfe2e05e53b5c68dc241e5a4615f80744
unknown <baudaegam@gmail.com>
2022-06-03 12:08:13 +0900

    Start Working on pokemon moves.

--- get_pokemon_data_from_web.py
+++ get_pokemon_data_from_web.py
@@ -5,20 +5,16 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver import ActionChains
 
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup as bs
 
 import re
 
 import pandas as pd
-import numpy as np
 
 
 def set_chrome_driver():
